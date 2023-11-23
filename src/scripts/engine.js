6d70b638837fb046306b93b7912dc3ce54e9579a
Eduardo Berbet de Alcantara <edu.berbet@hotmail.com>
2023-11-23 15:17:10 -0300

    Add files via upload

---
+++ src/scripts/engine.js
@@ -0,0 +1,56 @@
+const emojis = [
+   '<img class="emojis" src="./src/images/001.webp">',
+   '<img class="emojis" src="./src/images/001.webp">',
+   '<img class="emojis" src="./src/images/002.webp">',
+   '<img class="emojis" src="./src/images/002.webp">',
+   '<img class="emojis" src="./src/images/003.webp">',
+   '<img class="emojis" src="./src/images/003.webp">',
+   '<img class="emojis" src="./src/images/004.webp">',
+   '<img class="emojis" src="./src/images/004.webp">',
+   '<img class="emojis" src="./src/images/005.webp">',
+   '<img class="emojis" src="./src/images/005.webp">',
+   '<img class="emojis" src="./src/images/006.webp">',
+   '<img class="emojis" src="./src/images/006.webp">',
+   '<img class="emojis" src="./src/images/007.webp">',
+   '<img class="emojis" src="./src/images/007.webp">',
+   '<img class="emojis" src="./src/images/008.webp">',
+   '<img class="emojis" src="./src/images/008.webp">'
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.classList.add("item");
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+};
+
+function handleClick() {
+    if(openCards.length < 2 ){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    };
+
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    };
+};
+
+function checkMatch() {
+  if(openCards[0].innerHTML === openCards[1].innerHTML){
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  }else{
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+  openCards = [];
+  
+  if(document.querySelectorAll(".boxMatch").length === emojis.length){
+    alert("Você venceu!");
+  }
+};
